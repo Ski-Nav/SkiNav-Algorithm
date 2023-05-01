5c07b108874143cec7d31f8109d08c4cfbfa1229
zychen <zychen5186@gmail.com>
2023-04-30 20:54:49 -0700

    add comments on requestGraph.ts

--- src/utils/requestGraph.ts
+++ src/utils/requestGraph.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
 
+/**
+ * request graph via api, then store it in "../data/json/"
+ */
 export async function requestGraph(resortName: string) {
     const url = "http://ec2-18-222-140-238.us-east-2.compute.amazonaws.com:3000/api/v1/maps/".concat(resortName);
     const response = await fetch(url);
@@ -7,5 +10,3 @@ export async function requestGraph(resortName: string) {
     let fileName = __dirname.concat('/../data/json/', resortName, '_graph.json');
     fs.writeFileSync(fileName, jsonData);
 }
-
-requestGraph("UCSD");
