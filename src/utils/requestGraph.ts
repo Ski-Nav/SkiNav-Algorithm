7c8c9687c25c5aaf3ce6dc9a3b98c05d9f23d9d4
zychen <zychen5186@gmail.com>
2023-04-30 20:46:46 -0700

    update requestGraph.ts

--- src/utils/requestGraph.ts
+++ src/utils/requestGraph.ts
@@ -1,14 +1,11 @@
 import fs from 'fs';
 
-export async function fetchGraph(url: string) {
+export async function requestGraph(resortName: string) {
+    const url = "http://ec2-18-222-140-238.us-east-2.compute.amazonaws.com:3000/api/v1/maps/".concat(resortName);
     const response = await fetch(url);
     const jsonData = JSON.stringify(await response.json());
-    let urlSplit = url.split("/");
-    let name = urlSplit[urlSplit.length-1];
-    let fileName = __dirname.concat('/../data/json/', name, '_graph.json');
+    let fileName = __dirname.concat('/../data/json/', resortName, '_graph.json');
     fs.writeFileSync(fileName, jsonData);
 }
 
-
-
-fetchGraph("http://ec2-18-222-140-238.us-east-2.compute.amazonaws.com:3000/api/v1/maps/UCSD");
+requestGraph("UCSD");
